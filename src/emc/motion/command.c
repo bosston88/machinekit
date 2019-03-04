476405e45d5f41655794bccee195bc16f5803f2c
bosston88 <6061928+bosston88@users.noreply.github.com>
2019-03-04 19:52:36 +0100

    ReverseRun changes

--- src/emc/motion/command.c
+++ src/emc/motion/command.c
@@ -1095,7 +1095,21 @@ int emcmotCommandHandler(void *arg, const hal_funct_args_t *fa)
 	    emcmotStatus->resuming = 0;
 	    emcmotDebug->stepping = 0;
 	    break;
+			
+	case EMCMOT_REVERSE:
+	    /* run motion in reverse*/
+	    /* only allowed during a pause */
+	    rtapi_print_msg(RTAPI_MSG_DBG, "REVERSE");
+        tpSetRunDir(&emcmotDebug->tp, TC_DIR_REVERSE);
+	    break;
 
+	case EMCMOT_FORWARD:
+	    /* run motion in reverse*/
+	    /* only allowed during a pause */
+	    rtapi_print_msg(RTAPI_MSG_DBG, "FORWARD");
+        tpSetRunDir(&emcmotDebug->tp, TC_DIR_FORWARD);
+	    break;
+			
 	case EMCMOT_RESUME:
 	    /* resume paused motion */
 	    /* can happen at any time */
